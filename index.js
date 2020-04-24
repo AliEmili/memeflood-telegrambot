faab7d9986f5ceb26a3a418575688428e916cea4
Ali Z. Emili <aliemili2013@yahoo.com>
2020-04-24 22:22:10 +0430

    make it better

---
+++ index.js
@@ -0,0 +1,30 @@
+const telegraf = require('telegraf');
+require("dotenv").config();
+const Extra = require('telegraf/extra');
+const fetch = require("node-fetch");
+
+const token = process.env.BOT_TOKEN;
+const bot = new telegraf(token);
+
+bot.command('start', ctx => ctx.reply('Welcome'));
+
+bot.command('meme', async ctx => {
+    let src = await pic();
+    ctx.reply(`<a href="${src}">This is the link of the meme</a>`, Extra.HTML().markup((m) => m.inlineKeyboard([m.callbackButton('Next', 'next')])));
+});
+
+bot.action('next', async ctx => {
+    let src = await pic();
+    ctx.editMessageText(`<a href="${src}">This is the link of the meme</a>`, Extra.HTML().markup((m) => m.inlineKeyboard([m.callbackButton('Next', 'next')])));
+})
+
+// Generating picture
+async function pic() {
+    var url;
+    let response = await fetch('https://meme-api.herokuapp.com/gimme/1');
+    let data = await response.json();
+    let memes = await data.memes;
+    url = memes[0].url;
+    return url;
+}
+bot.launch();
