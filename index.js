55dc9c0f47f94659c68c88fcde6b9e15806e290d
Ali Z. Emili <aliemili2013@yahoo.com>
2020-04-25 11:33:39 +0430

    error handle

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-try {
-    const telegraf = require('telegraf');
-    require("dotenv").config();
-    const Extra = require('telegraf/extra');
-    const fetch = require("node-fetch");
+const telegraf = require('telegraf');
+require("dotenv").config();
+const Extra = require('telegraf/extra');
+const fetch = require("node-fetch");
 
-    const token = process.env.BOT_TOKEN;
-    const bot = new telegraf(token);
+const token = process.env.BOT_TOKEN;
+const bot = new telegraf(token);
 
-    bot.command('start', ctx => ctx.reply('Welcome'));
+bot.command('start', ctx => ctx.reply('Welcome'));
 
-    bot.command('meme', async ctx => {
-        let src = await pic();
-        ctx.reply(`<a href="${src}">This is the link of the meme</a>`, Extra.HTML().markup((m) => m.inlineKeyboard([m.callbackButton('Next', 'next')])));
-    });
+bot.command('meme', async ctx => {
+    let src = await pic();
+    ctx.reply(`<a href="${src}">This is the link of the meme</a>`, Extra.HTML().markup((m) => m.inlineKeyboard([m.callbackButton('Next', 'next')])));
+});
 
-    bot.action('next', async ctx => {
-        let src = await pic();
-        ctx.editMessageText(`<a href="${src}">This is the link of the meme</a>`, Extra.HTML().markup((m) => m.inlineKeyboard([m.callbackButton('Next', 'next')])));
-    })
+bot.action('next', async ctx => {
+    let src = await pic();
+    ctx.editMessageText(`<a href="${src}">This is the link of the meme</a>`, Extra.HTML().markup((m) => m.inlineKeyboard([m.callbackButton('Next', 'next')])));
+})
 
-    // Generating picture
-    async function pic() {
-        var url;
-        let response = await fetch('https://meme-api.herokuapp.com/gimme/1')
-            .catch(err => {
-                console.log(err);
-            })
-        let data = await response.json();
-        let memes = await data.memes;
-        url = memes[0].url;
-        return url;
+// Generating picture
+async function pic() {
+    var url;
+    let response = await fetch('https://meme-api.herokuapp.com/gimme/1')
+        .catch(err => {
+            console.log(err);
+        })
+    let data = await response.json();
+    let memes = await data.memes;
+    url = memes[0].url;
+    return url;
 
-    }
-    bot.launch();
-} catch (err) {
-    console.log(err);
 }
+bot.launch();
